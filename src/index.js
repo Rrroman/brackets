d436ed5fd36dcfb5577afd40c9b506fa62b30bcf
Roman Kohutyak <romankohutyak@gmail.com>
2020-10-09 23:52:34 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let brackets = [];
+    bracketsConfig.forEach((element) => {
+        brackets.push(element.join(""));
+    });
+
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i])) {
+            str = str.split(brackets[i]).join("");
+            i = -1;
+        }
+    }
+
+    if (str === "") {
+        return true;
+    } else {
+        return false;
+    }
+};
